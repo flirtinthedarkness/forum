6bc8fa2d31f0856f3c9b4bf93d6798ec31e1d869
NonZErO <theallenanker@gmail.com>
2020-02-27 18:02:45 +0800

    Added HomeController.

--- src/main/java/com/delicate/forum/controller/HomeController.java
+++ src/main/java/com/delicate/forum/controller/HomeController.java
@@ -1,4 +1,43 @@
 package com.delicate.forum.controller;
 
+import com.delicate.forum.entity.DiscussPost;
+import com.delicate.forum.entity.User;
+import com.delicate.forum.service.DiscussPostService;
+import com.delicate.forum.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model) {
+        List<DiscussPost> discussPostList = discussPostService.findDiscussPosts(0, 0, 10);
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if (discussPostList != null) {
+            for (DiscussPost post : discussPostList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", post);
+                User user = userService.findUserById(post.getUserId());
+                map.put("user", user);
+                discussPosts.add(map);
+            }
+        }
+        model.addAttribute("discussPosts", discussPosts);
+        return "/index";
+    }
 }
