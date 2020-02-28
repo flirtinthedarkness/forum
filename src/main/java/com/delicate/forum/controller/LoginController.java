7e2a40b1dfb4168eb8ba6c28a62b6435550deacc
NonZErO <theallenanker@gmail.com>
2020-02-28 17:03:40 +0800

    Add login page's verification code part(kaptcha).

--- src/main/java/com/delicate/forum/controller/LoginController.java
+++ src/main/java/com/delicate/forum/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.delicate.forum.controller;
 import com.delicate.forum.entity.User;
 import com.delicate.forum.service.UserService;
 import com.delicate.forum.util.ForumConstant;
+import com.google.code.kaptcha.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,14 +13,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/account")
 public class LoginController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -60,5 +74,23 @@ public class LoginController {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        // generate kaptcha verification code
+        String codeText = kaptchaProducer.createText();
+        BufferedImage codeImage = kaptchaProducer.createImage(codeText);
+
+        session.setAttribute("kaptcha", codeText);
+
+        response.setContentType("image/png");
+        try {
+            OutputStream os =  response.getOutputStream();
+            ImageIO.write(codeImage, "png", os);
+        } catch (IOException e) {
+            logger.error("Response verification code failed: " + e.getMessage());
+        }
+
     }
 }
