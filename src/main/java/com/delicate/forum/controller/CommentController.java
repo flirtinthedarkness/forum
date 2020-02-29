24dbbcd62627d962cd0b4e79304ada71cff71e03
NonZErO <theallenanker@gmail.com>
2020-02-29 20:22:11 +0800

    Add function "Comment Reply".

---
+++ src/main/java/com/delicate/forum/controller/CommentController.java
@@ -0,0 +1,33 @@
+package com.delicate.forum.controller;
+
+import com.delicate.forum.entity.Comment;
+import com.delicate.forum.service.CommentService;
+import com.delicate.forum.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int postId, Comment comment) {
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setStatus(0);
+        comment.setCreateTime(new Date());
+        commentService.addComment(comment);
+
+        return "redirect:/discuss/detail/" + postId;
+    }
+}
--- src/main/java/com/delicate/forum/service/CommentService.java
+++ src/main/java/com/delicate/forum/service/CommentService.java
@@ -2,8 +2,15 @@ package com.delicate.forum.service;
 
 import com.delicate.forum.dao.CommentMapper;
 import com.delicate.forum.entity.Comment;
+import com.delicate.forum.util.ForumConstant;
+import com.delicate.forum.util.HostHolder;
+import com.delicate.forum.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -13,11 +20,36 @@ public class CommentService {
     @Autowired
     private CommentMapper commentMapper;
 
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
     }
 
     public int getCommentCount(int entityType, int entityId) {
         return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+        if (comment == null) {
+            throw new IllegalArgumentException("Comment cannot be null");
+        }
+
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filterString(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        // update post comment count
+        if (comment.getEntityType() == ForumConstant.ENTITY_POST) {
+            int currCount = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), currCount);
+        }
+
+        return rows;
     }
 }
