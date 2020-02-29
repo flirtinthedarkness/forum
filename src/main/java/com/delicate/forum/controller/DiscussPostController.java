c0d1a47875033569e30c8fd3f8d2e28c500113bd
NonZErO <theallenanker@gmail.com>
2020-02-29 16:58:09 +0800

    Add "Post Detail" page.

--- src/main/java/com/delicate/forum/controller/DiscussPostController.java
+++ src/main/java/com/delicate/forum/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.delicate.forum.controller;
 import com.delicate.forum.entity.DiscussPost;
 import com.delicate.forum.entity.User;
 import com.delicate.forum.service.DiscussPostService;
+import com.delicate.forum.service.UserService;
 import com.delicate.forum.util.ForumUtils;
 import com.delicate.forum.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +25,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/addPost", method = RequestMethod.POST)
     @ResponseBody
@@ -39,5 +45,15 @@ public class DiscussPostController {
         discussPostService.addDiscussPost(post);
 
         return ForumUtils.getJSONString(200, "Post Succeed");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", discussPost);
+        User user = userService.findUserById(discussPost.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
     }
 }
